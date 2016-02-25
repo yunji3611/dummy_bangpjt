0fb2cfb39bd9bbf651e612398fb78b2a562ae3b1
YUNJI <a9a8a7a@naver.com>
2016-02-25 16:33:16 +0900

    회원가입 완료 !!

--- routes/members.js
+++ routes/members.js
@@ -1,13 +1,95 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
+var async = require('async');
 
 var router = express.Router();
+
 
 // 회원가입
 router.post('/', function (req, res, next) {
     if (req.secure) {
-        res.json({
-            "message": "가입이 정상적으로 처리되었습니다..."
-        });
+        //res.json({
+        //    "message": "가입이 정상적으로 처리되었습니다..."
+        //});
+
+        var username = req.body.username;
+        var email = req.body.email;
+        var password = req.body.password;
+
+        function getConnection(callback) {
+            pool.getConnection(function(err, connection) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, connection);
+                }
+            });
+        }
+
+        function selectMember(connection, callback) {
+            var sql = "SELECT email "+
+                      "FROM bangdb.user "+
+                      "WHERE email = ?";
+            connection.query(sql, [email], function (err, members) {
+                if (err) {
+                    connection.release();
+                    callback(err);
+                } else {
+                    if (members.length) {
+                        connection.release();
+                        var err = new Error('아이디 중복!!');
+                        err.status = 409;
+                        callback(err);
+                    } else {
+                        callback(null, connection);
+                    }
+                }
+            });
+        }
+
+        function generateSalt (connection, callback) {
+            var rounds = 10;    // 횟수 default : 10
+            bcrypt.genSalt(rounds, function (err, salt) {
+               if (err) {
+                   callback(err);
+               } else {
+                   callback(null, salt, connection);
+               }
+            });
+        }
+
+        function generateHashPassword (salt, connection, callback) {
+            bcrypt.hash(password, salt, function (err, hashPassword) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, hashPassword, connection)
+                }
+            })
+        }
+
+        function insertMember(hashPassword, connection, callback) {
+            var sql = "insert into bangdb.user(username, email, password) "+
+                       "values (?, ?, ?)";
+            connection.query(sql, [username, email, hashPassword], function (err, member) {
+                connection.release();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, member);
+                }
+            })
+        }
+
+        async.waterfall([getConnection, selectMember, generateSalt, generateHashPassword, insertMember], function (err, result) {
+            if (err) {
+                next(err);
+            } else {
+                result.message = "회원가입이 완료되었습니다."
+                res.json(result);
+            }
+        })
+
 
     } else {
         var err = new Error('SSL/TLS Upgrade Required');
@@ -21,7 +103,8 @@ router.post('/login', function (req, res, next) {
    if (req.secure) {
        res.json({
            "message": "로그인 되었습니다..."
-       })
+       });
+
    } else {
        var err = new Error('SSL/TLS Upgrade Required');
        err.status = 426;
