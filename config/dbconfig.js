3b26a49d5e73f86d8a9461c9e20acfb89cfd313d
YUNJI <a9a8a7a@naver.com>
2016-02-25 16:04:09 +0900

    db 연동

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,10 @@
+//var config = {
+//    "host": "bangserver.czdglqff2woq.ap-northeast-2.rds.amazonaws.com",
+//    "user": "bangadmin",
+//    "password": "00003611",
+//    "debug": true
+//}
+
 var config = {
     "host": "localhost",
     "user": "root",
