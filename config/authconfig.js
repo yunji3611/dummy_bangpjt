5ec09b02cc68c1c9fb567360e119e4162ec4ac99
YUNJI <a9a8a7a@naver.com>
2016-02-26 17:17:05 +0900

    facebook appId, appSecret

---
+++ config/authconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "facebook": {
+        "appId": "171184256591238",
+        "appSecret":"7d9473c4a3d3c7802be42ea23f6edeea",   // 안드로이드와 상의해서 바꿔
+        // "callbackURL": "http://localhost/auth/facebook/callback"
+    }
+}
