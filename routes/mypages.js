96bbea6f605915463bcff08692c60e51c4af3195
YUNJI <a9a8a7a@naver.com>
2016-03-02 18:52:52 +0900

    mypages조회

--- routes/mypages.js
+++ routes/mypages.js
@@ -1,6 +1,6 @@
 var express = require('express');
-
 var router = express.Router();
+var async = require('async');
 
 function isLoggedIn(req, res, next) {
     if (!req.isAuthenticated()) {
@@ -15,24 +15,56 @@ function isLoggedIn(req, res, next) {
 // 마이페이지 조회
 router.get('/', isLoggedIn, function (req, res, next) {
     if (req.secure) {
-
         var user = req.user;
-        res.json({
-            "message": "프로필이 조회되었습니다",
-            "date": {
-                "name": user.username,
-                "photo_url": "",
-                "myscrap_count": 5,
-                "mypost_count": 10
-            }
-        });
 
+        function getConnection(callback){
+            pool.getConnection(function (err, connection) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, connection);
+                }
+            })
+        }
+
+        function selectUser (connection, callback) {
+            var sql =   "SELECT username, photo_path "+
+                        "FROM bangdb.user "+
+                        "WHERE id = ?";
+            connection.query(sql, [user.id], function (err, info) {
+                connection.release();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, info)
+                }
+            })
+        }
+
+        function resultJSON (info, callback) {
+            var result = {
+                "username": info[0].username,
+                "photo_path": info[0].photo_path
+            };
+            callback(null, result);
+        }
+
+        async.waterfall([getConnection, selectUser, resultJSON], function (err, result) {
+            if (err) {
+                next(err);
+            } else {
+                res.json(result);
+            }
+        })
     } else {
         var err = new Error('SSL/TLS Upgrade Required');
         err.status = 426;
         next(err);
     }
 });
+
+
+
 
 // 마이페이지 프로필 사진 수정
 router.put('/', function (req, res, next) {
