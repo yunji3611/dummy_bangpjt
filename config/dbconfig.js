ef2b7f69b53e260fc68d8678c62136b1cdb326f0
YUNJI <a9a8a7a@naver.com>
2016-03-03 14:10:38 +0900

    ..

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,15 +1,15 @@
-//var config = {
-//    "host": "bangserver.czdglqff2woq.ap-northeast-2.rds.amazonaws.com",
-//    "user": "bangadmin",
-//    "password": "00003611",
-//    "debug": true
-//}
-
 var config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "0000",
+    "host": "bangserver.czdglqff2woq.ap-northeast-2.rds.amazonaws.com",
+    "user": "bangadmin",
+    "password": "00003611",
     "debug": true
 }
+
+//var config = {
+//    "host": "localhost",
+//    "user": "root",
+//    "password": "0000",
+//    "debug": true
+//}
 
 module.exports = config;
