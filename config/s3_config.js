3db17d6b23a3a0c27dfc5a2f92aca1e4c5b96843
YUNJI <a9a8a7a@naver.com>
2016-03-03 19:49:49 +0900

    s3_config

---
+++ config/s3_config.js
@@ -0,0 +1,12 @@
+var s3 = {
+    "key" :"AKIAIAWVMOE6SCKLSIUQ",
+    "secret" :"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",
+    "region" :"ap-northeast-2",
+    "bucket" :"bangpjt",
+    "imageACL": "public-read",
+    "mypages": {"imageDir": "mypages"},
+    "posts": {"imageDir": "posts"},
+    "furniture": {"imageDir": "furniture"}
+};
+
+module.exports = s3;
