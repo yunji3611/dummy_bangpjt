976533a580c3f9146ae1797c85103f723459d4a2
YUNJI <a9a8a7a@naver.com>
2016-03-04 18:44:01 +0900

    push 틀

--- routes/rentalpushes.js
+++ routes/rentalpushes.js
@@ -1,13 +1,54 @@
 var express = require('express');
-
+var gcm = require('node-gcm');
 var router = express.Router();
 
-// 반남기간 알림
-router.post('/', function (req, res, next) {
+function isLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        var err = new Error('로그인이 필요합니다');
+        err.status = 401;
+        next(err);
+    } else {
+        next();
+    }
+}
 
-        res.json({
-            "message": "반납기간 알림이 완료되었습니다..."
-        })
+// 임대 알림
+router.post('/', isLoggedIn, function (req, res, next) {
+
+    var message = new gcm.Message({
+        collapseKey: 'demo',
+        delayWhileIdle: true,
+        timeToLive: 3,
+        data: {
+            lecture_id:"notice",
+            title:"임대기간알림",
+            desc: "임대기간이 일주일 남았습니다"
+        }
+    });
+
+    var server_access_key = "안드로이드 개발자가 넘겨준 서버키";
+    var sender = new gcm.Sender(server_access_key);
+    var registrationIds = [];
+    var registration_id = "안드로이드 registration_id 값";
+    registrationIds.push(registration_id);
+
+// Send to a topic, with no retry this time
+    sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, response) {
+        if(err) {
+            console.error(err);
+        } else {
+            console.log(response);
+        }
+    });
+
+    sender.send(message, registrationIds, 4, function (err, response) {
+        if(err) {
+            console.error(err);
+        } else {
+            console.log(response);
+        }
+    });
+
 
 });
 
--- routes/replypushes.js
+++ routes/replypushes.js
@@ -1,13 +1,75 @@
 var express = require('express');
-
+var gcm = require('node-gcm');
 var router = express.Router();
 
-// 댓글 알림
-router.post('/', function (req, res, next) {
 
-        res.json({
-            "message": "댓글 푸시 알림이 완료되었습니다..."
+function isLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        var err = new Error('로그인이 필요합니다');
+        err.status = 401;
+        next(err);
+    } else {
+        next();
+    }
+}
+
+
+// 댓글 알림
+router.post('/', isLoggedIn, function (req, res, next) {
+
+    var user = req.user;
+
+    function getConnection (connection, callback) {
+        pool.getConnection(function (err, callback) {
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, connection);
+            }
+        })
+    }
+
+
+
+    if ("내 게시글에 댓글 달렸을때") {
+        var message = new gcm.Message({
+            collapseKey: 'demo',
+            delayWhileIdle: true,
+            timeToLive: 3,
+            data: {
+                lecture_id:"notice",
+                title:"댓글알림",
+                desc: "해당 게시글에 댓글이 등록되었습니다"
+            }
         });
+
+        var server_access_key = "안드로이드 개발자가 넘겨준 서버키";
+        var sender = new gcm.Sender(server_access_key);
+        var registrationIds = [];
+        var registration_id = "안드로이드 registration_id 값";
+        registrationIds.push(registration_id);
+
+        // Send to a topic, with no retry this time
+        sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, response) {
+            if(err) {
+                console.error(err);
+            } else {
+                console.log(response);
+            }
+        });
+
+        sender.send(message, registrationIds, 4, function (err, response) {
+            if(err) {
+                console.error(err);
+            } else {
+                console.log(response);
+            }
+        });
+    } else {
+
+    }
+
+
 
 });
 
