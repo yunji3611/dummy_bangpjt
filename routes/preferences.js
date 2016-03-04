b7e9ab0ad47f320e589230311f50707c4c1a4abd
YUNJI <a9a8a7a@naver.com>
2016-03-04 16:07:52 +0900

    취향조사질문,답변조회

--- routes/preferences.js
+++ routes/preferences.js
@@ -1,19 +1,68 @@
 var express = require('express');
-
+var async = require('async');
 var router = express.Router();
 
 // 취향조사
 router.get('/', function (req, res, next) {
-    res.json({
-        "message": "취향조사 내용이 전달되었습니다..",
-        "data": [{"questionary_id": 1,
-                  "questionary": "당신의 나이대는?",
-                  "item_seq": 1,
-                   "item": "20"},
-                {"questionary_id": 1,
-                "questionary": "어떤 스타일을 좋아해요?",
-                "item_seq": 2,
-                "item": "도시적인 스타일"}]
+
+    function getConnection (callback) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                callback (err);
+            } else {
+                callback(null, connection);
+            }
+        })
+    }
+
+    function selectQuestion (connection, callback) {
+        var sql = "SELECT q.question as question, i.questionary_id as qid, i.item_seq as iseq, i.item as item "+
+                  "FROM bangdb.questionary q JOIN bangdb.item i "+
+                  "ON (q.id = i.questionary_id)";
+        connection.query(sql, [], function(err, questions) {
+            connection.release();
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, questions);
+            }
+        })
+    }
+
+    function resultJSON (questions, callback) {
+        var preferList = [];
+        var index =0;
+        async.each(questions, function(item, callback) {
+            var result = {
+                "questionary_id": questions[index]["qid"],
+                "questionary": questions[index]["question"],
+                "item_seq": questions[index]["iseq"],
+                "item": questions[index]["item"]
+            };
+            index++;
+            preferList.push(result);
+            callback(null);
+
+        }, function(err){
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, preferList);
+            }
+        });
+    }
+
+    async.waterfall([getConnection, selectQuestion, resultJSON], function (err, preferList) {
+        if (err) {
+            next(err);
+        } else {
+            res.json({
+                "result" : {
+                    "message" : "취향조사 설문내용이 전달되었습니다",
+                    "preferData": preferList
+                }
+            })
+        }
     })
 });
 
