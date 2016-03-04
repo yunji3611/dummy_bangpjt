a66507c9ff4775856ba49cc3d72c0bb98c46679b
minju <minju236@naver.com>
2016-03-04 16:56:29 +0900

    게시물목록

--- routes/posts.js
+++ routes/posts.js
@@ -17,21 +17,19 @@ router.get('/', function (req, res, next) {
     var page = parseInt(req.query.page);
     page = isNaN(page) ? 1 : page;
     post_type = post_type > 1 ? 0 : post_type;
+    //var category =
 
-
-    var postList = [];
-    var limit = 12;
+    //var postList = [];
+    var limit = 10;
     var offset = (page - 1) * limit;
     function selectPosts(connection, callback) {
 
         var post1 = "select p.id, p.content, p.category, u.username, u.photo_path, fi.file_path " +
-                    "from post p join (select id, username, photo_path from user) u "+
-                    "on(p.user_id = u.id) " +
-                    "join file fi on(p.id = fi.post_id) " +
-                    "join hashtag_has_post hp on (p.id = hp.post_id) " +
-                    "join hashtag h on (h.id = hp.hashtag_id) "
-                    "where post_type = ? " +
-                    "limit? offset ? ";
+          "from post p left join file fi on(p.id = fi.post_id) " +
+          "left join (select id, username, photo_path from user) u " +
+          "on(p.user_id = u.id) " +
+          "where post_type =0 " +
+          "limit ? offset ?  ";
 
         connection.query(post1, [limit, offset], function (err, results) {
             if (err) {
@@ -48,29 +46,29 @@ router.get('/', function (req, res, next) {
           "join post p on(p.id=hp.post_id) " +
           "where post_type= ? and  p.id= ?";
 
-        var furniture = "select f.fphoto_path, f.brand, f.name, f.no, f.size, f.color_id, f.link "+
-                        "from furniture f join post p on(f.post_id = p.id) " +
-                        "join color c on(f.color_id = c.id) "+
-                        "where post_type= ? and p.id = ? ";
+        var furniture = "select f.fphoto_path as furniture_url, f.brand, f.name, f.no, f.size, f.color_id, f.link "+
+          "from furniture f join post p on(f.post_id = p.id) " +
+          "join color c on(f.color_id = c.id) "+
+          "where post_type= ? and p.id = ? ";
 
-        var reply = "select  r.username as r_username, r.reply_content, r.reply_time " +
+        var reply = "select  r.username as username, r.reply_content, r.reply_time " +
           "from reply r join post p on(r.post_id = p.id) " +
           "where post_type= ? and p.id = ? ";
 
         var index = 0;
         async.eachSeries(results, function (element, cb1) {
             async.series([function (cb2) {
-                connection.query(hashtag, [post_type, element.id], function (err, tag_results) {
+                connection.query(hashtag, [post_type, element.post_id], function (err, tag_results) {
                     if (err) {
                         cb2(err);
                     } else {
-                        results[index].tag = tag_results;
+                        results[index].hash_tag = tag_results;
                         cb2(null);
                     }
                 });
 
             }, function (cb2) {
-                connection.query(furniture, [post_type, element.id], function (err, furniture_results) {
+                connection.query(furniture, [post_type, element.post_id], function (err, furniture_results) {
                     if (err) {
                         cb2(err);
                     } else {
@@ -80,19 +78,20 @@ router.get('/', function (req, res, next) {
                     }
                 });
             }, function (cb2) {
-                connection.query(reply, [post_type, element.id], function (err, reply_results) {
+                connection.query(reply, [post_type, element.post_id], function (err, reply_results) {
                     if (err) {
                         cb2(err);
                     } else {
-                        results[index].replys = reply_results;
+                        results[index].replies = reply_results;
                         cb2(null);
                     }
                 });
-            }], function (err, index) {
+            }], function (err) {
                 if (err) {
                     cb1(err);
                 } else {
                     index++;
+                    console.log(index);
                     cb1(null);
                 }
             });
@@ -107,62 +106,60 @@ router.get('/', function (req, res, next) {
         });
     }
 
-function resultJSON(results, callback) {
-    var postList = [];
+    function resultJSON(results, callback) {
+        var postList = [];
 
 
-    async.forEach(results, function (results, cb) {
-        var postresult = {
+        async.eachSeries(results, function (results, cb) {
+            var postresult = {
 
                 "post_id": results.id,
                 "photo_url": results.photo_path,
-                "interior_url": results.file_path,
+                "file_url": results.file_path,
                 "scrap_count": "확인",
-              // "post_count": "",
-                "hashtag": results.tag,
+                // "post_count": "",
+                "hash_tag": results.tag,
                 "category": results.category,
-                "furniture": results.furnitures,
+                "furnitures": results.furnitures,
                 "content": results.content,
-                "reply_username": results.replys.r_username,
-                "reply": results.replys
+                //"reply_username": results.replys.username,
+                "reply": results.replies
 
-
-        };
-        postList.push(postresult);
-        cb(null);
-
-    }, function (err) {
-        if (err) {
-            callback(err);
-        } else {
-            var results =
-            {
-                "result": {
-                    "message": "게시물 목록이 조회되었습니다.",
-                    "data": {
-                        //count: results.length,
-                        "page": page,
-                        "listperPage": limit
-                    },
-                    "postList": postList
-                }
 
             };
-            callback(null, results);
-        }
-    });
-}
-    if (req.query.post_type) {
+            postList.push(postresult);
+            cb(null);
 
-        async.waterfall([getConnection, selectPosts, selectPosts2], function (err, result) {
+        }, function (err) {
             if (err) {
-                next(err); //처리도중문제이거나 쿼리수행증..// 뭇
+                callback(err);
             } else {
-                res.json(result);
+                var results =
+                {
+                    "result": {
+                        "message": "게시물 목록이 조회되었습니다.",
+                        "postData": {
+                            "page": page,
+                            "listperPage": limit
+                        },
+                        "postList": postList
+                    }
 
+                };
+                callback(null, results);
             }
         });
     }
+
+    async.waterfall([getConnection, selectPosts, selectPosts2, resultJSON], function (err, result) {
+        if (err) {
+            next(err); //처리도중문제이거나 쿼리수행증..// 뭇
+        } else {
+            res.json(result);
+
+        }
+    });
+
 });
 
 
@@ -194,7 +191,7 @@ router.post('/', isLoggedIn, function (req, res, next) {
 
     function insertPost(connection, callback) {
         var sql = "insert into post (post_type, content,category, package, user_id) "+
-                  "values (?, ?, ?, ?, ?)";
+          "values (?, ?, ?, ?, ?)";
         var values = [];
         values.push(req.body.post_type);
         values.push(req.body.content);
@@ -212,13 +209,13 @@ router.post('/', isLoggedIn, function (req, res, next) {
             }
         });
     }
-  async.waterfall([getConnection, insertPost], function (err, result) {
-      if (err) {
-          next(err);
-      } else {
-          res.json(result);
-      }
-  });
+    async.waterfall([getConnection, insertPost], function (err, result) {
+        if (err) {
+            next(err);
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 
@@ -240,7 +237,7 @@ router.put('/', function (req, res, next) {
 router.delete('/', function (req, res, next) {
 
     var user = { "id" : req.user.id ,
-                 "post_id" : req.query.post_id };
+        "post_id" : req.query.post_id };
     console.log('userid2:' ,user.id);
 
     function getConnection(callback) {
@@ -254,8 +251,8 @@ router.delete('/', function (req, res, next) {
     }
     function compareUserId (connection, callback) {
         var sql = "select user_id " +
-        "from post "+
-        "where id = ? ";
+          "from post "+
+          "where id = ? ";
         connection.query(sql, [user.post_id], function(err, user_results) {
 
             console.log('userresults:',user_results[0].user_id);
@@ -274,7 +271,7 @@ router.delete('/', function (req, res, next) {
     }
     function deletePost(connection, callback) {
         var deletesql = "delete from post " +
-                        "where id = ? ";
+          "where id = ? ";
 
         connection.query(deletesql, [user.post_id], function(err, result) {
             connection.release();
@@ -321,7 +318,7 @@ router.post('/:post_id/replies', isLoggedIn, function (req, res, next) {
 
     function insertReply(connection, callback) {
         var sql = "insert into reply (username, reply_time, reply_content, post_id) " +
-        "values (?, now(), ?, ?) ";
+          "values (?, now(), ?, ?) ";
         var values = [];
         values.push(user.username);
         values.push(req.body.reply_content);
@@ -369,193 +366,50 @@ router.delete('/:post_id/replies/:reply_id', isLoggedIn, function (req, res, nex
     }
 
     function compareUser(connection, callback) {
-        var sql = "";
-        connection.query(sql, [], function (err, results) {
+        var sql = "select u.id as userid, u.username, r.post_id as rpostid, r.id as replyid " +
+          "from reply r join (select id, username " +
+          "from user) u "+
+          "on (u.username = r.username) " +
+          "where u.id=? and r.id=? and post_id=?";
+        connection.query(sql, [user.id, rid, pid], function (err, results) {
             if (err) {
                 connection.release();
-                callback(err);
-            } else {
-
-            }
-        });
-    }
-    var result = {
-        "result": {
-            "message": "게시물의 댓글이 삭제되었습니다."
-        }
-    }
-    res.json(result);
-
-});
-
-//var test = parseInt(1);
-//console.log(test);
-router.get('/post', function (req, res, next) {
-    function getConnection(callback) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
                 callback(err);
             } else {
                 callback(null, connection);
             }
         });
     }
-    var tag = req.query.tag;
-    //var post_type = parseInt(req.query.post_type);
-    var page = parseInt(req.query.page);
-    page = isNaN(page) ? 1 : page;
-    //post_type = post_type > 1 ? 0 : post_type;
-    console.log('tag:', tag);
-    console.log(page);
-
-
-    var limit = 10;
-    var offset = (page - 1) * limit;
-    function selectPosts(connection, callback) {
-
-        var post1 = "select p.id, p.content, p.category, u.username, u.photo_path, fi.file_path " +
-          "from post p join (select id, username, photo_path from user) u "+
-          "on(p.user_id = u.id) " +
-          "join file fi on(p.id = fi.post_id) "+
-          "join hashtag_has_post hp on (p.id = hp.post_id) "+
-          "join hashtag h on (h.id = hp.hashtag_id) " ;
-        if(tag != null) {
-            //console.log('nn');
-            var select = "where h.tag like " + '"%' + tag + '%"';
-            post1 += select;
-            post1 += "limit ? offset ? ";
-            //console.log(select);
-
-        }
-        connection.query(post1, [limit, offset], function (err, results) {
-            if (err) {
-                callback(err);
-            } else {
-                callback(null, connection, results);
-            }
-        });
-    }
-
-    function selectPosts2(connection, results, callback) {
-        var hashtag = "select h.tag, hp.post_id " +
-          "from hashtag_has_post hp join hashtag h on (h.id = hp.hashtag_id) " +
-          "join post p on(p.id=hp.post_id) " +
-          "where p.id= ?";
-
-        var furniture = "select p.id, f.fphoto_path, f.brand, f.name, f.no, f.size, f.color_id, f.link "+
-          "from furniture f join post p on(f.post_id = p.id) " +
-          "join color c on(f.color_id = c.id) "+
-          "where p.id = ? ";
-
-        var reply = "select p.id,  r.username as r_username, r.reply_content, r.reply_time " +
-          "from reply r join post p on(r.post_id = p.id) " +
-          "where  p.id = ? ";
-
-        var index = 0;
-        async.eachSeries(results, function (element, cb1) {
-            async.series([function (cb2) {
-                connection.query(hashtag, [element.id], function (err, tag_results) {
-                    if (err) {
-                        cb2(err);
-                    } else {
-                        results[index].tag = tag_results;
-                        cb2(null);
-                    }
-                });
-
-            }, function (cb2) {
-                connection.query(furniture, [ element.id], function (err, furniture_results) {
-                    if (err) {
-                        cb2(err);
-                    } else {
-                        results[index].furnitures = furniture_results;
-                        console.log(furniture_results);
-                        cb2(null);
-                    }
-                });
-            }, function (cb2) {
-                connection.query(reply, [ element.id], function (err, reply_results) {
-                    if (err) {
-                        cb2(err);
-                    } else {
-                        results[index].replys = reply_results;
-                        cb2(null);
-                    }
-                });
-            }], function (err) {
-                if (err) {
-                    cb1(err);
-                } else {
-                    index++;
-                    cb1(null);
-                }
-            });
-        }, function (err) {
+    function deleteReply(connection, callback) {
+        var deletesql = "delete from reply where id = ? " ;
+        connection.query(deletesql, [rid], function (err, r_results) {
             connection.release();
             if (err) {
                 callback(err);
             } else {
+                callback(null);
 
-                callback(null, results);
             }
         });
     }
-
-    function resultJSON(results, callback) {
-        var postList = [];
-        async.eachSeries(results, function (results, cb) {
-            var postresult = {
-
-                "post_id": results.id,
-                "photo_url": results.photo_path,
-                "interior_url": results.file_path,
-                "scrap_count": "확인",
-                // "post_count": "",
-                "hashtag": results.tag,
-                "category": results.category,
-                "furniture_url": results.fphoto_path,
-                "furniture": results.furnitures,
-                "content": results.content,
-                "reply_username": results.replys.r_username,
-                "reply": results.replys
-
-
-            };
-            postList.push(postresult);
-            cb(null);
-
-        }, function (err) {
-            if (err) {
-                callback(err);
-            } else {
-                var results =
-                {
-                    "result": {
-                        "message": "해당 검색에어대한 게시물 목록이 조회되었습니다.",
-                        "data": {
-                            //"count": results.length,
-                            "page": page,
-                            "listperPage": limit
-                        },
-                        "postList": postList
-                    }
-
-                };
-                callback(null, results);
-            }
-        });
-    }
-
-
-    async.waterfall([getConnection, selectPosts, selectPosts2, resultJSON], function (err, result) {
+    async.waterfall([getConnection, compareUser, deleteReply], function(err, result) {
         if (err) {
-            next(err); //처리도중문제이거나 쿼리수행증..// 뭇
+            next(err);
         } else {
+            var result = {
+                "result": {
+                    "message": "게시물의 댓글이 삭제되었습니다."
+                }
+            };
             res.json(result);
-
         }
     });
+
 });
+
+//var test = parseInt(1);
+//console.log(test);
+
 
 
 module.exports = router;
