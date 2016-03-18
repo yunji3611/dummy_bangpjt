aa74d519b79733a4c5766b2377a76182b2d1a9ae
YUNJI <a9a8a7a@naver.com>
2016-03-18 12:14:47 +0900

    push

--- routes/rentalpushes.js
+++ routes/rentalpushes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var gcm = require('node-gcm');
 var async = require('async');
 var passport = require('passport');
-var moment = require('moment');
+var moment = require('moment-timezone');
 var nodeschedule = require('node-schedule');
 var router = express.Router();
 
@@ -28,33 +28,45 @@ router.get('/:orderId', function (req, res, next) {
                    "FROM bangdb.orders o LEFT JOIN bangdb.user u ON (o.user_id = u.id) "+
                    "WHERE o.id =?";
         connection.query(sql, [reqPost], function (err, order) {
-            connection.release();
             if (err) {
+                connection.release();
                 err.message = "select registration token 에러발생 ";
                 err.code = "E00007";
                 callback(err);
             } else {
-                callback(null, order);
+                callback(null, order, connection);
             }
         });
     }
 
-    function sendPush(order, callback) {
+    function sendPush(order, connection, callback) {
         console.log('===> registration_token :' + order[0].registration_token);
-        console.log('year==>'+order[0].year +'year==>'+order[0].month +'year==>'+order[0].day-7)
+        console.log('year==>'+order[0].year +' month==>'+order[0].month +' day==>'+order[0].day);
         var year = order[0].year;
         var month = order[0].month;
-        var day = order[0].day - 7;
+        var day = parseInt(order[0].day) - parseInt(7);
         var hour = 13;
         var minute = 0;
         var second = 0;
 
         var m = moment({"year": year, "month": month, "day": day,
-                "hour": hour, "minute": minute, "second": second}).tz('Asia/Seoul');
-        //var date= m.format(YYYYMMDD);
+                "hour": hour, "minute": minute, "second": second}).tz('Asia/Seoul').format();
+        console.log('m===>'+m);
+        var mSql =  "UPDATE orders "+
+                    "SET push_time= '"+m+"' "+
+                    "WHERE id=?";
+        connection.query(mSql, [reqPost], function (err) {
+            if (err) {
+                var err = new Error('push time등록 에러발생');
+                err.code = "E00007";
+                callback(err);
+            } else {
+
+            }
+        });
 
         var job = nodeschedule.scheduleJob(m, function () {
-
+            console.log('job들어왔음===');
             // push 알람 실행
             var message = new gcm.Message({
                 collapseKey: 'demo',
@@ -73,30 +85,50 @@ router.get('/:orderId', function (req, res, next) {
             var server_access_key = "AIzaSyCTqs_tFwUjY-HUEj_tM01nH7Yfg4uBlVE";
             var sender = new gcm.Sender(server_access_key);
             var registrationIds = [];
-            var registration_id = key[0].registration_token;
+            var registration_id = order[0].registration_token;
             registrationIds.push(registration_id);
 
             sender.send(message, registrationIds, function (err, response) {
                 if (err) {
-                    var err = new Error("send에러 발생했습니다");
-                    err.code = "E00007";
-                    callback(err);
+                    // err 상태 저장
+                    var errSql = "UPDATE orders "+
+                                 "SET push_state= 'err' "+
+                                 "WHERE id=?";
+                    connection.query(errSql, [reqPost], function (err) {
+                        connection.release();
+                        if (err) {
+
+                        } else {
+
+                        }
+                    });
                 } else {
                     console.log(response);
-                    callback(null);
+                    // push보냈는지 저장
+                    var successSql = "UPDATE orders "+
+                                     "SET push_state= 'send' "+
+                                     "WHERE id=?";
+                    connection.query(successSql, [reqPost], function (err) {
+                        connection.release();
+                        if (err) {
+
+                        } else {
+
+                        }
+                    });
                 }
             });
             // push
-
-
         });
+
+        callback(null);
     }
 
-    async.waterfall([getConnection, selectKey, sendPush], function (err, result) {
+    async.waterfall([getConnection, selectKey, sendPush], function (err) {
         if (err) {
             next(err);
         } else {
-            res.json({"message": "임대 푸시 알람이 전송되었습니다"});
+            res.json({"message": "임대 푸시 알람이 등록되었습니다"});
         }
     })
 
